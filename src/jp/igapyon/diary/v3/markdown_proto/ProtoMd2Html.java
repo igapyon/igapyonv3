d891d6093018a948d34bc33efaa7ba70d6a28ea7
Toshiki Iga <tosiki.iga@nifty.ne.jp>
2015-06-27 22:54:12 +0900

    Trying to markdown.

--- src/jp/igapyon/diary/v3/markdown_proto/ProtoMd2Html.java
+++ src/jp/igapyon/diary/v3/markdown_proto/ProtoMd2Html.java
@@ -1,7 +1,11 @@
 package jp.igapyon.diary.v3.markdown_proto;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
 
 import org.eclipse.mylyn.wikitext.core.parser.MarkupParser;
@@ -9,12 +13,30 @@ import org.eclipse.mylyn.wikitext.core.parser.builder.HtmlDocumentBuilder;
 import org.eclipse.mylyn.wikitext.markdown.core.MarkdownLanguage;
 
 public class ProtoMd2Html {
+	public static String readTextFile(final File file) throws IOException {
+		final StringWriter writer = new StringWriter();
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(
+				new FileInputStream(file), "UTF-8"));
+		for (;;) {
+			final String line = reader.readLine();
+			if (line == null) {
+				break;
+			}
+			writer.write(line);
+			writer.write("\n");
+		}
+		reader.close();
+		writer.close();
+		return writer.toString();
+	}
+
 	public static void main(final String[] args) throws IOException {
 		final StringWriter strWriter = new StringWriter();
 		final BufferedWriter writer = new BufferedWriter(strWriter);
 		final MarkupParser parser = new MarkupParser(new MarkdownLanguage(),
 				new HtmlDocumentBuilder(writer));
-		parser.parse("An h1 header\n============\nParagraphs are separated by a blank line.\n  * this one");
+		parser.parse(readTextFile(new File(
+				"./src/jp/igapyon/diary/v3/markdown_proto/test001.md")));
 
 		writer.close();
 		System.out.println(strWriter.toString());
