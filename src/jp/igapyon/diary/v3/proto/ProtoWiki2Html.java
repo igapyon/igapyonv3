0adad2b691f9780b12e682d0cbe968eab48c1a4c
Toshiki Iga <tosiki.iga@nifty.ne.jp>
2015-06-27 17:46:44 +0900

    Learning Bliki with sample codes.

--- src/jp/igapyon/diary/v3/proto/ProtoWiki2Html.java
+++ src/jp/igapyon/diary/v3/proto/ProtoWiki2Html.java
@@ -1,7 +1,5 @@
 package jp.igapyon.diary.v3.proto;
 
-import info.bliki.wiki.model.WikiModel;
-
 import java.io.IOException;
 import java.io.StringWriter;
 
@@ -15,7 +13,8 @@ public class ProtoWiki2Html {
 	public static void main(final String[] args) {
 		final StringWriter writer = new StringWriter();
 		try {
-			WikiModel.toHtml("もんげーシンプルな [[Hello World]] wiki タグなう.", writer);
+			IgapyonWikiModel.toHtml("もんげーシンプルな [[Hello World]] wiki タグなう.",
+					writer);
 			writer.flush();
 			writer.close();
 		} catch (IOException ex) {
