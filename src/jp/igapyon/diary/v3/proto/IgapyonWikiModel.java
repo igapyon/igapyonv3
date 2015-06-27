e4c342362bc4440d4f485bb9f339f9c681216933
Toshiki Iga <tosiki.iga@nifty.ne.jp>
2015-06-27 17:54:10 +0900

    Learning Bliki sources.

--- src/jp/igapyon/diary/v3/proto/IgapyonWikiModel.java
+++ src/jp/igapyon/diary/v3/proto/IgapyonWikiModel.java
@@ -1,8 +1,19 @@
 package jp.igapyon.diary.v3.proto;
 
+import info.bliki.htmlcleaner.TagNode;
 import info.bliki.wiki.model.WikiModel;
 
 public class IgapyonWikiModel extends WikiModel {
+	static {
+		// see:
+		// https://bitbucket.org/axelclk/info.bliki.wiki/wiki/Mediawiki2HTML
+		//
+		// By default the rendering engine doesn't allow the style attribute to
+		// avoid cross-site scripting risks. You can define the style attribute
+		// as allowed in a static block of your WikiModel implementation.
+		TagNode.addAllowedAttribute("style");
+	}
+
 	public IgapyonWikiModel(final String imageBaseURL, final String linkBaseURL) {
 		super(imageBaseURL, linkBaseURL);
 		// TODO Auto-generated constructor stub
