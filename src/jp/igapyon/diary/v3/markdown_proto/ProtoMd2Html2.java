1eee44c1f13e3cddd163bcb9dbee770399b329f4
Toshiki Iga <tosiki.iga@nifty.ne.jp>
2015-06-28 21:50:53 +0900

    adding surrownd html.

--- src/jp/igapyon/diary/v3/markdown_proto/ProtoMd2Html2.java
+++ src/jp/igapyon/diary/v3/markdown_proto/ProtoMd2Html2.java
@@ -2,6 +2,7 @@ package jp.igapyon.diary.v3.markdown_proto;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 
 import jp.igapyon.diary.v3.util.IgapyonV3Util;
 
@@ -10,12 +11,36 @@ import org.pegdown.PegDownProcessor;
 
 public class ProtoMd2Html2 {
 	public static void main(final String[] args) throws IOException {
+		final StringWriter writer = new StringWriter();
+		writer.write("<!DOCTYPE html>\n");
+		writer.write("<html lang=\"ja\">\n");
+		writer.write("<head>\n");
+		writer.write("<meta charset=\"utf-8\">\n");
+		writer.write("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n");
+		writer.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
+		writer.write("<meta name=\"description\" content=\"\">\n");
+		writer.write("<meta name=\"author\" content=\"\">\n");
+		writer.write("<title>タイトル</title>\n");
+		writer.write("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">\n");
+		writer.write("</head>\n");
+		writer.write("<body>\n");
+		writer.write("<div class=\"container\">");
+
 		final PegDownProcessor processor = new PegDownProcessor(
 				Extensions.FENCED_CODE_BLOCKS /* , PegDownPlugins */);
 		String aaa = processor
 				.markdownToHtml(IgapyonV3Util.readTextFile(new File(
 						"./src/jp/igapyon/diary/v3/markdown_proto/test001.md")));
+		writer.write(aaa);
 
-		System.out.println(aaa);
+		writer.write("</div>\n");
+
+		writer.write("<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>\n");
+		writer.write("<script src=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>\n");
+		writer.write("</body>\n");
+		writer.write("</html>\n");
+
+		writer.close();
+		System.out.println(writer.toString());
 	}
 }
