69c1a083887975afed3b214d0748cc8ecdebc0ae
Toshiki Iga <tosiki.iga@nifty.ne.jp>
2015-07-02 00:59:19 +0900

    Trial for icon.

--- src/jp/igapyon/diary/v3/util/IgapyonV3Util.java
+++ src/jp/igapyon/diary/v3/util/IgapyonV3Util.java
@@ -91,12 +91,13 @@ public class IgapyonV3Util {
 		writer.write("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js\"></script>\n");
 		writer.write("<!-- Compiled and minified JavaScript -->\n");
 		writer.write("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>\n");
+
+		// TODO Should be preset on HTML tag class.
 		writer.write("<script>\n");
 		writer.write("$(function() {\n");
-		writer.write("  $(\"h2\").addClass(\"alert alert-warning\");\n");
-		writer.write("  $(\"h3\").addClass(\"bg-success\");\n");
+		writer.write("  $(\"h2\").addClass(\"alert alert-warning glyphicon glyphicon-leaf\");\n");
+		writer.write("  $(\"h3\").addClass(\"bg-success glyphicon glyphicon-grain\");\n");
 		writer.write("  $(\"table\").addClass(\"table table-bordered\");\n");
-
 		writer.write("});\n");
 		writer.write("</script>\n");
 
