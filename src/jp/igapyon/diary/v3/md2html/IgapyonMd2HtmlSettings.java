d24678592c0f4bd3fdc9e0107f7622bed1a8ba33
Toshiki Iga <tosiki.iga@nifty.ne.jp>
2015-07-08 22:56:24 +0900

    Adding settings class.

--- src/jp/igapyon/diary/v3/md2html/IgapyonMd2HtmlSettings.java
+++ src/jp/igapyon/diary/v3/md2html/IgapyonMd2HtmlSettings.java
@@ -31,8 +31,6 @@
  *********************************************************************** */
 package jp.igapyon.diary.v3.md2html;
 
-public class IgapyonMd2HtmlConstants {
-	public static final String PROGRAM_DISPLAY_NAME = "Igapyon Diary System v3 (igapyonv3)";
+public class IgapyonMd2HtmlSettings {
 
-	public static final String VERSION = "010";
 }
