a03e60a38221a97dc953a9983d3cc2962d14321f
Toshiki Iga <igapyon@gmail.com>
2016-12-31 18:03:22 +0900

    save

--- src/main/java/jp/igapyon/diary/v3/mdconv/ConvertDiarySrcMd2Md.java
+++ src/main/java/jp/igapyon/diary/v3/mdconv/ConvertDiarySrcMd2Md.java
@@ -10,21 +10,7 @@ import org.apache.commons.io.FileUtils;
  * .src.md から .md を生成するためのクラス。
  */
 public class ConvertDiarySrcMd2Md {
-	public void process() throws IOException {
-		File dir = new File(".");
-		dir = dir.getCanonicalFile();
-		System.out.println(dir.getPath());
-
-		if (dir.getName().equals("diary")) {
-			System.out.println("期待通りディレクトリ");
-			processDir(dir);
-		} else {
-			System.out.println("期待とは違うディレクトリ:" + dir.getName());
-			return;
-		}
-	}
-
-	void processDir(final File dir) throws IOException {
+	public void processDir(final File dir) throws IOException {
 		final File[] files = dir.listFiles();
 		if (files == null) {
 			return;
@@ -34,7 +20,6 @@ public class ConvertDiarySrcMd2Md {
 				processDir(file);
 			} else if (file.isFile()) {
 				if (file.getName().startsWith("ig") && file.getName().endsWith(".src.md")) {
-//					System.out.println(file.getName());
 					processFile(file);
 				}
 			}
@@ -62,6 +47,8 @@ public class ConvertDiarySrcMd2Md {
 			// System.out.println(" " + line);
 		}
 
+		// TODO support template system.
+
 		// ヘッダ追加
 		lines.add(0, year1 + year2 + "-" + month + "-" + day + " diary: " + firstH2Line);
 		lines.add(1,
@@ -80,10 +67,9 @@ public class ConvertDiarySrcMd2Md {
 		lines.add("");
 		lines.add("## この日記について");
 		lines.add(
-				"[いがぴょんについて](http://www.igapyon.jp/igapyon/diary/memo/memoigapyon.html) / [インデックスに戻る](https://igapyon.github.io/diary/idxall.html)");
+				"[いがぴょんについて](https://igapyon.github.io/diary/memo/memoigapyon.html) / [インデックスに戻る](https://igapyon.github.io/diary/idxall.html)");
 
 		String newName = file.getName().substring(0, file.getName().length() - (".src.md".length())) + ".md";
 		FileUtils.writeLines(new File(file.getParentFile(), newName), lines);
-		// System.out.println("NEWMANE [" + newName + "]");
 	}
 }
