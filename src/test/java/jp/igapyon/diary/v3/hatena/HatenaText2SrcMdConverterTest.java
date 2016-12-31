8110792479f4632dc609d2f20ec50971611a2e4f
Toshiki Iga <igapyon@gmail.com>
2016-12-31 19:45:24 +0900

    save

--- src/test/java/jp/igapyon/diary/v3/hatena/HatenaText2SrcMdConverterTest.java
+++ src/test/java/jp/igapyon/diary/v3/hatena/HatenaText2SrcMdConverterTest.java
@@ -4,10 +4,16 @@ import java.io.File;
 
 import org.junit.Test;
 
+import jp.igapyon.diary.v3.mdconv.DiarySrcMd2MdConverter;
+
 public class HatenaText2SrcMdConverterTest {
 
 	@Test
 	public void test() throws Exception {
-		new HatenaText2SrcMdConverter().processDir(new File("./test/data/hatena/"));
+		final File rootdir = new File("./test/data/hatena/");
+
+		new HatenaText2SrcMdConverter().processDir(rootdir);
+
+		new DiarySrcMd2MdConverter().processDir(rootdir);
 	}
 }
