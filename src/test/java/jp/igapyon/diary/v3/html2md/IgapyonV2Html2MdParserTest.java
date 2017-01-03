15ce50145ea4eb36d0553db9996e6ab620acfa7a
Toshiki Iga <igapyon@gmail.com>
2017-01-03 20:07:42 +0900

    save

--- src/test/java/jp/igapyon/diary/v3/html2md/IgapyonV2Html2MdParserTest.java
+++ src/test/java/jp/igapyon/diary/v3/html2md/IgapyonV2Html2MdParserTest.java
@@ -8,6 +8,6 @@ public class IgapyonV2Html2MdParserTest {
 
 	@Test
 	public void test() throws Exception {
-		IgapyonV2Html2MdUtil.convertV2Html2Md(new File("./test/data/v2html/ig100102.html").getCanonicalFile());
+		IgapyonV2Html2MdUtil.convertV2Html2Md(new File("./test/data/v2html/ig100102-orig.html").getCanonicalFile());
 	}
 }
