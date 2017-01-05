7b58b7688a517343a7cfdb053d10dafd38d88a9a
Toshiki Iga <igapyon@gmail.com>
2017-01-05 22:07:24 +0900

    save

--- src/main/java/jp/igapyon/diary/v3/mdconv/IgapyonV3TemplateLoader.java
+++ src/main/java/jp/igapyon/diary/v3/mdconv/IgapyonV3TemplateLoader.java
@@ -169,7 +169,7 @@ public class IgapyonV3TemplateLoader implements TemplateLoader {
 			String header = "[top](https://igapyon.github.io/diary/) \n";
 			header += " / [index](https://igapyon.github.io/diary/" + year1 + year2 + "/index.html) \n";
 
-			if (entryIndex <= 0 || entryIndex > synEntryList.size() - 2) {
+			if (entryIndex < 0 || entryIndex > synEntryList.size() - 2) {
 				header += " / prev \n";
 			} else {
 				header += " / [prev](" + synEntryList.get(entryIndex + 1).getLink() + ") \n";
