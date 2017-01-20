8cd888a0dc17fb84bb80cc4cae72b239b0280900
Toshiki Iga <igapyon@gmail.com>
2017-01-21 08:35:57 +0900

    savey

--- src/main/java/jp/igapyon/diary/v3/mdconv/freemarker/directive/LocalYearlistDirectiveModel.java
+++ src/main/java/jp/igapyon/diary/v3/mdconv/freemarker/directive/LocalYearlistDirectiveModel.java
@@ -83,10 +83,40 @@ public class LocalYearlistDirectiveModel implements TemplateDirectiveModel {
 			}
 		};
 		final List<File> files = parser.listFiles(settings.getRootdir(), false);
-		for (File file : files) {
-			System.out.println(file.getAbsolutePath());
+
+		for (int index = files.size() - 1; index >= 0; index--) {
+			final File file = files.get(index);
+			writer.write("[" + file.getName() + "](" + settings.getBaseurl() + "/" + file.getName() + "/index.html)\n");
 		}
+
+		writer.write("/ [ALL](" + settings.getBaseurl() + "/idxall.html)\n");
 
 		writer.flush();
 	}
+
+	public static String getYearListMdString() {
+		return "[2017](https://igapyon.github.io/diary/2017/index.html)\n"
+				+ "/ [2016](https://igapyon.github.io/diary/2016/index.html)\n"
+				+ "/ [2015](https://igapyon.github.io/diary/2015/index.html)\n"
+				+ "/ [2014](https://igapyon.github.io/diary/2014/index.html)\n"
+				+ "/ [2013](https://igapyon.github.io/diary/2013/index.html)\n"
+				+ "/ [2012](https://igapyon.github.io/diary/2012/index.html)\n"
+				+ "/ [2011](https://igapyon.github.io/diary/2011/index.html)\n"
+				+ "/ [2010](https://igapyon.github.io/diary/2010/index.html)\n"
+				+ "/ [2009](https://igapyon.github.io/diary/2009/index.html)\n"
+				+ "/ [2008](https://igapyon.github.io/diary/2008/index.html)\n"
+				+ "/ [2007](https://igapyon.github.io/diary/2007/index.html)\n"
+				+ "/ [2006](https://igapyon.github.io/diary/2006/index.html)\n"
+				+ "/ [2005](https://igapyon.github.io/diary/2005/index.html)\n"
+				+ "/ [2004](https://igapyon.github.io/diary/2004/index.html)\n"
+				+ "/ [2003](https://igapyon.github.io/diary/2003/index.html)\n"
+				+ "/ [2002](https://igapyon.github.io/diary/2002/index.html)\n"
+				+ "/ [2001](https://igapyon.github.io/diary/2001/index.html)\n"
+				+ "/ [2000](https://igapyon.github.io/diary/2000/index.html)\n"
+				+ "/ [1998](https://igapyon.github.io/diary/1998/index.html)\n"
+				+ "/ [1997](https://igapyon.github.io/diary/1997/index.html)\n"
+				+ "/ [1996](https://igapyon.github.io/diary/1996/index.html)\n"
+				+ "/ [ALL](https://igapyon.github.io/diary/idxall.html)\n";
+	}
+
 }
--- test/data/directive/localyearlist/test_localyearlist.html.md
+++ test/data/directive/localyearlist/test_localyearlist.html.md
@@ -6,6 +6,8 @@ Test
 
 ## Test
 
+[2017](https://igapyon.github.io/diary/2017/index.html)
+/ [ALL](https://igapyon.github.io/diary/idxall.html)
 
 
 ----------------------------------------------------------------------------------------------------
