589235e8723c05e96a91cda4ab9930801bf871d1
Toshiki Iga <igapyon@gmail.com>
2017-01-22 23:35:57 +0900

    fix bug.

--- src/main/java/jp/igapyon/diary/v3/mdconv/freemarker/IgapyonV3FreeMarkerUtil.java
+++ src/main/java/jp/igapyon/diary/v3/mdconv/freemarker/IgapyonV3FreeMarkerUtil.java
@@ -38,6 +38,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -176,6 +177,20 @@ public class IgapyonV3FreeMarkerUtil {
 					String word = mat.group();
 					word = word.substring(2, word.length() - 2);
 					current.getKeywordList().add(word);
+				}
+			}
+			{
+				// 重複キーワードは除去
+				final Map<String, String> mapDupCheck = new HashMap<String, String>();
+				for (int index = 0; index < current.getKeywordList().size(); index++) {
+					final String lookup = current.getKeywordList().get(index);
+					if (mapDupCheck.get(lookup.toLowerCase()) != null) {
+						// dup key
+						current.getKeywordList().remove(index);
+						index--;
+						continue;
+					}
+					mapDupCheck.put(lookup.toLowerCase(), lookup);
 				}
 			}
 		}
