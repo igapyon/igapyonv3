c823be6350598aee7ecdd1e995ec6c53c9d96344
Toshiki Iga <igapyon@gmail.com>
2017-01-29 08:57:30 +0900

    save

---
+++ src/test/java/jp/igapyon/diary/v3/DefaultProcessorTest.java
@@ -0,0 +1,10 @@
+package jp.igapyon.diary.v3;
+
+import org.junit.Test;
+
+public class DefaultProcessorTest {
+	@Test
+	public void test() throws Exception {
+		DefaultProcessor.main(new String[] {});
+	}
+}
