a37b11289b86f84b0fd011d824b38f14100d066f
igapyon <igapyon@mac.lan>
2026-01-04 18:52:47 +0900

    バグ対応。

--- src/main/java/jp/igapyon/diary/igapyonv3/md2html/IgapyonMd2HtmlConstants.java
+++ src/main/java/jp/igapyon/diary/igapyonv3/md2html/IgapyonMd2HtmlConstants.java
@@ -40,5 +40,5 @@ package jp.igapyon.diary.igapyonv3.md2html;
 public class IgapyonMd2HtmlConstants {
 	public static final String PROGRAM_DISPLAY_NAME = "Igapyon Diary System v3 (igapyonv3)";
 
-	public static final String VERSION = "2.0.20260104.2";
+	public static final String VERSION = "2.0.20260104.3";
 }
